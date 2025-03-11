fc09679ccbfe6587d1c65cf4ddf4993ce97f81da
mu2550 <40687@prot.ac.th>
2025-03-11 11:56:21 +0700

    ex03

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,12 @@
+num = 1
+
+while num <= 12:
+    multiplier = 1
+    
+    while multiplier <= 12:
+        print(f"{num} x {multiplier} = {num * multiplier}")
+        multiplier += 1
+    
+    print()
+    num += 1
+input()
