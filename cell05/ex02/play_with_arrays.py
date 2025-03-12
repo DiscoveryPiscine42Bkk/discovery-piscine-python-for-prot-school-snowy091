7826e6876694dade7b13419bc1abe657ad3cf7f0
mu2550 <40687@prot.ac.th>
2025-03-12 13:16:43 +0700

    ex02

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,15 @@
+
+def play_with_arrays():
+   
+    original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+    
+
+    new_array = [num * 5 for num in original_array]
+    
+
+    print("Original array:", original_array)
+    print("New array:", new_array)
+
+if __name__ == "__main__":
+    play_with_arrays()
+
